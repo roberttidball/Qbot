305d31bccd8b61b3cec0b0b6df670bd69410b595
Robert Tidball <info@fxmacrodata.com>
2026-07-09 00:28:27 +1000

    Expand FXMacroData read endpoint coverage

--- qbot/data/__init__.py
+++ qbot/data/__init__.py
@@ -1,5 +1,19 @@
 """Data-source helpers for Qbot."""
 
-from qbot.data.fxmacrodata_calendar import load_fxmacrodata_calendar
+from qbot.data.fxmacrodata_calendar import (
+    FXMacroDataClient,
+    load_fxmacrodata_announcements,
+    load_fxmacrodata_calendar,
+    load_fxmacrodata_dataset,
+    load_fxmacrodata_forex,
+    load_fxmacrodata_predictions,
+)
 
-__all__ = ["load_fxmacrodata_calendar"]
+__all__ = [
+    "FXMacroDataClient",
+    "load_fxmacrodata_announcements",
+    "load_fxmacrodata_calendar",
+    "load_fxmacrodata_dataset",
+    "load_fxmacrodata_forex",
+    "load_fxmacrodata_predictions",
+]
